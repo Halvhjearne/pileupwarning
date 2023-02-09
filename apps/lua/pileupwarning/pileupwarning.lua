6923d7e558d3fa04f858847e26885e6cbb314690
Halvhjearne <halvhjearne@hotmail.com>
2023-02-09 03:32:49 +0100

    first commit

---
+++ apps/lua/pileupwarning/pileupwarning.lua
@@ -0,0 +1,118 @@
+--[[
+------------------------------------------------------------------
+| pileupwarning                                                  |
+| lua app for warning about stopped traffic in ac by Halvhjearne |
+------------------------------------------------------------------
+| this is a free app and may not be used in any commercial       |
+| way without written permission from Halvhjearne                |
+------------------------------------------------------------------
+--]]
+
+local playercar = ac.getCar(0)
+local sim = ac.getSim()
+
+local pileupsettings = ac.storage{
+    minspeed = 40,
+    mindist = 0,
+    signsize = 100,
+    maxdist = 1250,
+    angle = 90,
+    mincars = 2,
+    alwaysshowwarning = false,
+    debug = false
+}
+
+local function isinfront(spos, sdir, tpos)
+--    local dirtot = math.atan((target.position.y - source.position.y), (target.position.x - source.position.x))*(180 / math.pi)
+    local dirtot = ac.getCompassAngle(vec3(tpos.x - spos.x,tpos.y - spos.y,tpos.z - spos.z))
+    local returnval = dirtot-sdir
+    if returnval > 180 then
+        returnval = returnval-360
+    elseif returnval < 180*-1 then
+        returnval = returnval+360
+    end
+    local isfront = true
+    if returnval > pileupsettings.angle or returnval < pileupsettings.angle*-1 then
+        isfront =  false
+    end
+    return isfront, dirtot, returnval
+end
+
+function script.pileupwarning(dt)
+    local showwarning = false
+    if pileupsettings.alwaysshowwarning then showwarning = true end
+    local num = 0
+    local mycar = playercar
+    if sim.focusedCar > 0 then
+        mycar = ac.getCar(sim.focusedCar)
+    end
+    for i = 1, sim.carsCount - 1 do
+        local car = ac.getCar(i)
+        if (car.isHidingLabels or car.isAIControlled) and car.isConnected then
+            local distancefromme = car.position:distance(mycar.position)
+            local infront, dirtot, returnval = isinfront(mycar.position,mycar.compass,car.position)
+            if pileupsettings.debug then
+                ui.bulletText('car: '..i..' - Km/h: '..math.floor(car.speedKmh)..' - Dir to car: '..math.floor(dirtot)..' - returnval: '..math.floor(returnval)..' - infront: '..tostring(infront)..' - Distance: '..math.floor(distancefromme))
+            end
+            if infront and car.speedKmh <= pileupsettings.minspeed and distancefromme >= pileupsettings.mindist and distancefromme <= pileupsettings.maxdist then
+                if pileupsettings.debug then ui.sameLine(0,5) ui.text(' - (triggered)') end
+                num = num+1
+                if num >= pileupsettings.mincars then
+                    showwarning = true
+                end
+            end
+        end
+    end
+    if showwarning then
+        ui.image('pileup.png',vec2(pileupsettings.signsize,pileupsettings.signsize))
+    end
+end
+
+function script.pileupwarningMain(dt)
+    ui.separator()
+    pileupsettings.minspeed = ui.slider('Min speed', pileupsettings.minspeed, 0, 200, '%.0fKmh')
+    if ui.itemHovered() then ui.setTooltip('Speeds above this can not trigger the warning') end
+    pileupsettings.mindist = ui.slider('Min distance', pileupsettings.mindist, 0, pileupsettings.maxdist-1, '%.0fm')
+    if ui.itemHovered() then ui.setTooltip('Distance below this can not trigger the warning') end
+    pileupsettings.maxdist = ui.slider('Max distance', pileupsettings.maxdist, pileupsettings.mindist+1, 10000, '%.0fm')
+    if ui.itemHovered() then ui.setTooltip('Distance above this can not trigger the warning') end
+    pileupsettings.angle = ui.slider('Angle', pileupsettings.angle, 0, 180, '%.1f')
+    if ui.itemHovered() then ui.setTooltip('Angles around the cars gps direction (both sides), where ai can trigger the warning') end
+    pileupsettings.mincars = ui.slider('Min cars', pileupsettings.mincars, 1, 10, '%.0f')
+    if ui.itemHovered() then ui.setTooltip('Minimum amount of cars to be triggered, before the warning will be triggered') end
+    ui.separator()
+    pileupsettings.signsize = ui.slider('Warning size', pileupsettings.signsize, 25, 500, '%.0fx'..pileupsettings.signsize)
+    if ui.itemHovered() then ui.setTooltip('Size of the warning sign') end
+    if ui.checkbox('Always show warning', pileupsettings.alwaysshowwarning) then
+        if ui.keyboardButtonDown(ui.KeyIndex.Control) and ui.keyboardButtonDown(ui.KeyIndex.Shift) then
+            pileupsettings.debug = not pileupsettings.debug
+        else
+            pileupsettings.alwaysshowwarning = not pileupsettings.alwaysshowwarning
+        end
+    end
+    if ui.itemHovered() then ui.setTooltip('Always shows warning to see and place it on the screen') end
+    ui.separator()
+    if pileupsettings.debug then
+        local mycar = playercar
+        if sim.focusedCar > 0 then
+            mycar = ac.getCar(sim.focusedCar)
+        end
+        local t = {
+            ['sim.focusedCar: '] = sim.focusedCar,
+            ['car compass: '] = math.floor(mycar.compass),
+            ['car pos: '] = tostring(mycar.position),
+            ['car speed: '] = math.floor(mycar.speedKmh),
+            ['cam pos: '] = tostring(ac.getCameraPosition()),
+            ['cam compass: '] = math.floor(ac.getCompassAngle(ac.getCameraForward()))
+        }
+        for key, value in pairs(t) do
+            ui.bulletText(key..value)
+        end
+    end
+
+    ui.labelText('','* pileupwarning by Halvhjearne!')
+end
+
+--function script.update(dt)
+--    ac.setMessage('test','test test')
+--end
